13d9b299951b42aee14f3495dc76b46f33c0fc29
Ashish Jha <ashishjha.mymail@gmail.com>
2021-09-15 20:35:26 +0200

    Add Service Registry

--- messaging-service/src/main/kotlin/com/ramble/messaging/MessagingServiceApplication.kt
+++ messaging-service/src/main/kotlin/com/ramble/messaging/MessagingServiceApplication.kt
@@ -2,8 +2,10 @@ package com.ramble.messaging
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class MessagingServiceApplication
 
 fun main(args: Array<String>) {
--- user-identity-service/user-identity/src/main/kotlin/com/ramble/identity/IdentityServiceApplication.kt
+++ user-identity-service/user-identity/src/main/kotlin/com/ramble/identity/IdentityServiceApplication.kt
@@ -3,10 +3,12 @@ package com.ramble.identity
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication(scanBasePackages = ["com.ramble"])
+@EnableEurekaClient
 @EnableRedisRepositories(basePackages = ["com.ramble.accesstoken"])
 @EnableJpaRepositories(basePackages = ["com.ramble.token", "com.ramble.identity"])
 @EntityScan(value = ["com.ramble.token.*", "com.ramble.accesstoken.*", "com.ramble.identity.*"])
